59965f7707c57d66cd352b9c07590c79f2fa9fee
AlessandroMuscio <muscioalex30@gmail.com>
2021-05-13 10:48:31 +0200

    Completata classe settimana

--- src/alessandromuscio/polverisottili/ProgramMenu.java
+++ src/alessandromuscio/polverisottili/ProgramMenu.java
@@ -1,7 +1,14 @@
 package alessandromuscio.polverisottili;
 
 import it.unibs.fp.mylib.Menu;
+import it.unibs.fp.mylib.InputDati;
 
 public class ProgramMenu extends Menu {
+  public static void inserimentoValori() {
+    int i;
+    double[] valori_giornalieri = new double[Settimana.getGiorniInUnaSettimana()];
 
+    for(i=0;i<Settimana.getGiorniInUnaSettimana(); i++)
+      valori_giornalieri[i] = I;
+  }
 }
